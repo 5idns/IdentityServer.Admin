f285e3e3bd88b6c9b8a8331c716d13f355518d38
Leo <leo@leo>
2022-04-06 17:54:26 +0800

    添加Ingress认证模块

---
+++ src/Skoruba.Duende.IdentityServer.Admin.UI/Areas/AdminUI/Controllers/IngressController.cs
@@ -0,0 +1,31 @@
+﻿using Duende.IdentityServer.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Skoruba.Duende.IdentityServer.Admin.UI.Areas.AdminUI.Controllers
+{
+    public class IngressController : BaseController
+    {
+        public IngressController(ILogger<IngressController> logger) : base(logger)
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return Json(new
+                {
+                    authenticated = true,
+                    user = User.GetDisplayName()
+                });
+            }
+            return Json(new
+            {
+                authenticated = false,
+                user = ""
+            });
+        }
+    }
+}
